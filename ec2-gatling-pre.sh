0de1300ac405addf8d8cf98fe3daa68173984857
qiushuo222 <13443515+qiushuo222@users.noreply.github.com>
2022-03-23 21:14:52 -0700

    Update ec2-gatling-pre.sh

--- ec2-gatling-pre.sh
+++ ec2-gatling-pre.sh
@@ -1,3 +1,4 @@
+chmod 755 gatling-100*.sh
 curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
